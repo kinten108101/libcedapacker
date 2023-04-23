343a8613c956f731bae9e85caefba181891e65f0
kinten108101 <kinten108101@protonmail.com>
2023-04-23 23:03:30 +0700

    chore: code format

--- src/extract.c
+++ src/extract.c
@@ -17,114 +17,114 @@
 static inline int
 read_string (FILE *f, char *str) 
 {
-	char cur_char;
-	int len = 0;
-	for (;;)
-	{
-		fread (&cur_char,1,1,f);
-		if (!cur_char) break;
-		len++;
-		*(str++)=cur_char;
-	}
-	*str='\0';
-	return len;
+  char cur_char;
+  int len = 0;
+  for (;;)
+  {
+    fread (&cur_char,1,1,f);
+    if (!cur_char) break;
+    len++;
+    *(str++)=cur_char;
+  }
+  *str='\0';
+  return len;
 }
 
 void 
-write_tree_iter	(const char* path,	
+write_tree_iter (const char* path,  
                  FILE* f,
                  unsigned int tree_size, 
                  unsigned int offset, 
                  unsigned int length)
 {
-	char content [length];
-	long prev_offset = ftell (f);
+  char content [length];
+  long prev_offset = ftell (f);
 
-	fseek (f, SIZE_VPKHEADER + tree_size + offset, SEEK_SET);
-	fread (content,1,length,f);
-	fseek (f, prev_offset, SEEK_SET);
+  fseek (f, SIZE_VPKHEADER + tree_size + offset, SEEK_SET);
+  fread (content,1,length,f);
+  fseek (f, prev_offset, SEEK_SET);
     
-	mk_mkdir (path);
-	FILE *f_out = fopen (path,"w");
-	if (f_out==NULL)
-		mkdie ("Something is wrong!\n");
-	fwrite (content, 1, length, f_out);
+  mk_mkdir (path);
+  FILE *f_out = fopen (path,"w");
+  if (f_out==NULL)
+    mkdie ("Something is wrong!\n");
+  fwrite (content, 1, length, f_out);
 
-	fclose (f_out);
+  fclose (f_out);
 }
 
 void
 _pathcat (char *path_buffer,
-		  const char *base,
-		  const char *dir,
-		  const char *name,
-		  const char *ext)
+      const char *base,
+      const char *dir,
+      const char *name,
+      const char *ext)
 {
-	path_buffer[0] = '\0';
-	char *p = path_buffer;
-	p = mk_strcat(p, base);
+  path_buffer[0] = '\0';
+  char *p = path_buffer;
+  p = mk_strcat(p, base);
 
-	p = mk_strcat(p, "/");
-	if ( mk_strcmp(dir, " ") == 0 ) goto files;
-	p = mk_strcat(p,dir);
-	p = mk_strcat(p,"/");
-	files :
-	p = mk_strcat(p,name);
-	p = mk_strcat(p,".");
-	p = mk_strcat(p,ext);
+  p = mk_strcat(p, "/");
+  if ( mk_strcmp(dir, " ") == 0 ) goto files;
+  p = mk_strcat(p,dir);
+  p = mk_strcat(p,"/");
+  files :
+  p = mk_strcat(p,name);
+  p = mk_strcat(p,".");
+  p = mk_strcat(p,ext);
 }
 
 void
 write_tree (int size_tree, FILE* stream_fvpk) 
 {
-	while (1)
-	{
-		char ext [SIZE_FEXT];
-		int readlen = read_string (stream_fvpk, &ext[0]);
-		if (!readlen) break;
-		while (1)
-		{
-			char dir [SIZE_FDIR];
-			int readlen = read_string (stream_fvpk, &dir[0]);
-			if (!readlen) break;
-			while (1)
-			{
-				char name [SIZE_FNAME];
-				int readlen = read_string(stream_fvpk, &name[0]);
-				if (!readlen) break;
-			
-				char path[200];
-				_pathcat(&path[0], BASEDIR, dir, name, ext);
-				
-				VpkDirEntry entry;
-				fread(&entry, 1, SIZE_VPKENTRY, stream_fvpk);
-				write_tree_iter	(path,	
-								stream_fvpk,
-								size_tree,
-								entry.entry_offset,
-								entry.entry_size);
-			}
-		}
-	}
+  while (1)
+  {
+    char ext [SIZE_FEXT];
+    int readlen = read_string (stream_fvpk, &ext[0]);
+    if (!readlen) break;
+    while (1)
+    {
+      char dir [SIZE_FDIR];
+      int readlen = read_string (stream_fvpk, &dir[0]);
+      if (!readlen) break;
+      while (1)
+      {
+        char name [SIZE_FNAME];
+        int readlen = read_string(stream_fvpk, &name[0]);
+        if (!readlen) break;
+      
+        char path[200];
+        _pathcat(&path[0], BASEDIR, dir, name, ext);
+        
+        VpkDirEntry entry;
+        fread(&entry, 1, SIZE_VPKENTRY, stream_fvpk);
+        write_tree_iter (path,  
+                stream_fvpk,
+                size_tree,
+                entry.entry_offset,
+                entry.entry_size);
+      }
+    }
+  }
 }
 
 VpkDirHeader*
 read_header (FILE* f)
 {
-	VpkDirHeader* header = (VpkDirHeader*)malloc(SIZE_VPKHEADER);
-	fread(header,1,SIZE_VPKHEADER,f);
-	// count vs step
-	return header;
+  VpkDirHeader* header = (VpkDirHeader*)malloc(SIZE_VPKHEADER);
+  fread(header,1,SIZE_VPKHEADER,f);
+  // count vs step
+  return header;
 }
 
 void
 extract_pak (FILE* f)
 {
-	VpkDirHeader* header = read_header(f);
-	{
-		printf ("Singature: 0x%04x\n",header->sig);
-		printf ("ver: %d\n",header->ver);
-		printf ("Tree size (bytes): %d\n",header->tree_size);
-	}
-	write_tree(header->tree_size,f);
+  VpkDirHeader* header = read_header(f);
+  {
+    printf ("Singature: 0x%04x\n",header->sig);
+    printf ("ver: %d\n",header->ver);
+    printf ("Tree size (bytes): %d\n",header->tree_size);
+  }
+  write_tree(header->tree_size,f);
 }
--- src/vpk.h
+++ src/vpk.h
@@ -60,42 +60,42 @@
 
 typedef struct VpkDirHeader
 {
-	const unsigned int sig;
-	const unsigned int ver;
-	const unsigned int tree_size;
+  const unsigned int sig;
+  const unsigned int ver;
+  const unsigned int tree_size;
 } VpkDirHeader;
 
 typedef struct VpkDirEntry
 {
-	const unsigned int crc; // A 32bit crc of the file's data.
-	const unsigned short preload_size; // The number of bytes contained in the index file.
-	// A zero based index of the archive this file's data is contained in.
-	// If 0x7fff, the data follows the f_dir.
-	const unsigned short archive_idx;
-	// If archive_idx is 0x7fff, the offset of the file data relative to the end of the f_dir (see the header for more details).
-	// Otherwise, the offset of the data from the start of the specified archive.
-	const unsigned int entry_offset;
-	// If zero, the entire file is stored in the preload data.
-	// Otherwise, the number of bytes stored starting at entry_offset.
-	const unsigned int entry_size;
-	const unsigned short Terminator;
+  const unsigned int crc; // A 32bit crc of the file's data.
+  const unsigned short preload_size; // The number of bytes contained in the index file.
+  // A zero based index of the archive this file's data is contained in.
+  // If 0x7fff, the data follows the f_dir.
+  const unsigned short archive_idx;
+  // If archive_idx is 0x7fff, the offset of the file data relative to the end of the f_dir (see the header for more details).
+  // Otherwise, the offset of the data from the start of the specified archive.
+  const unsigned int entry_offset;
+  // If zero, the entire file is stored in the preload data.
+  // Otherwise, the number of bytes stored starting at entry_offset.
+  const unsigned int entry_size;
+  const unsigned short Terminator;
 } VpkDirEntry;
 
 typedef struct VpkFile VpkFile;
 struct VpkFile
 {
-	const char* ext;
-	const char* name;
-	const char* dir;
+  const char* ext;
+  const char* name;
+  const char* dir;
 };
 
 typedef struct hashlist
 {
-	// max 2^16=65536, whereas there are at most
-	// 1000 files in a common vpk. We use MAX_FILES 2048
-	unsigned short int len;
-	unsigned short int** list;
-	// byte size: 10 or 16 instead of 20 (stack overflow)
+  // max 2^16=65536, whereas there are at most
+  // 1000 files in a common vpk. We use MAX_FILES 2048
+  unsigned short int len;
+  unsigned short int** list;
+  // byte size: 10 or 16 instead of 20 (stack overflow)
 } hashlist;
 // Pascal integer array. A string that starts with a len counter.
 // Cost 4 more bytes but blazingly fast.
